474b4bed5f0b03da88cb4ca0c4e8ba31236a9d8e
bosankus <ankushbose5@gmail.com>
2024-11-24 10:54:37 +0530

    Fix: Language screen UI

--- language/src/main/java/bose/ankush/language/presentation/LanguageScreen.kt
+++ language/src/main/java/bose/ankush/language/presentation/LanguageScreen.kt
@@ -2,14 +2,18 @@ package bose.ankush.language.presentation
 
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,13 +21,19 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.EmojiSupportMatch
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import bose.ankush.language.R
 import bose.ankush.language.util.LocaleHelper.changeLanguageTo
+import bose.ankush.language.util.LocaleHelper.getCountryFlag
 import bose.ankush.language.util.LocaleHelper.getDisplayName
 
 @Composable
@@ -35,12 +45,10 @@ fun LanguageScreen(
         modifier = Modifier.fillMaxSize()
     ) {
         Scaffold(
-            topBar = { SettingsHeader(navAction) },
+            topBar = { ScreenHeader(navAction) },
             content = { innerPadding ->
-                Column(
-                    modifier = Modifier.padding(innerPadding)
-                ) {
-                    LanguageChangeSetting(languages = languages)
+                Column(modifier = Modifier.padding(innerPadding)) {
+                    ShowUI(languages = languages)
                 }
             }
         )
@@ -49,11 +57,11 @@ fun LanguageScreen(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun SettingsHeader(navAction: () -> Unit) {
+private fun ScreenHeader(navAction: () -> Unit) {
     TopAppBar(
         title = {
             Text(
-                text = stringResource(id = R.string.back_content_description),
+                text = stringResource(id = R.string.lib_screen_header),
                 style = MaterialTheme.typography.headlineSmall,
                 color = MaterialTheme.colorScheme.onBackground,
                 modifier = Modifier.padding(start = 16.dp)
@@ -75,27 +83,43 @@ private fun SettingsHeader(navAction: () -> Unit) {
 
 
 @Composable
-private fun LanguageChangeSetting(languages: Array<String>) {
+private fun ShowUI(languages: Array<String>) {
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth()
             .padding(start = 16.dp, end = 16.dp)
     ) {
         items(languages.size) { position ->
-            Text(
-                text = languages[position].getDisplayName(),
-                style = MaterialTheme.typography.bodyLarge,
-                color = MaterialTheme.colorScheme.onBackground,
+            Row(
                 modifier = Modifier
-                    .fillMaxWidth()
                     .padding(top = 16.dp)
                     .clip(RoundedCornerShape(5.dp))
-                    .clickable {
-                        val localeListCompat = changeLanguageTo(languages[position])
-                        Log.d("LanguageScreen", "LanguageChangeSetting: $localeListCompat")
-                    }
-                    .padding(all = 5.dp)
-            )
+                    .padding(all = 5.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    modifier = Modifier
+                        .clickable {
+                            val localeListCompat = changeLanguageTo(languages[position])
+                            Log.d("LanguageScreen", "LanguageChangeSetting: $localeListCompat")
+                        }
+                        .weight(1f),
+                    text = languages[position].let { "${it.getCountryFlag()}  ${it.getDisplayName()}" },
+                    style = TextStyle(
+                        platformStyle = PlatformTextStyle(
+                            emojiSupportMatch = EmojiSupportMatch.None
+                        ),
+                        color = MaterialTheme.colorScheme.onBackground,
+                    )
+                )
+                Icon(
+                    modifier = Modifier.alpha(0f), // TODO: Should be visible if item is selected
+                    imageVector = Icons.Filled.Check,
+                    tint = MaterialTheme.colorScheme.onBackground,
+                    contentDescription = "${languages[position]} ${Icons.Filled.Check.name}"
+                )
+            }
         }
     }
 }
--- language/src/main/java/bose/ankush/language/util/LocaleHelper.kt
+++ language/src/main/java/bose/ankush/language/util/LocaleHelper.kt
@@ -6,6 +6,16 @@ import java.util.Locale
 
 internal object LocaleHelper {
 
+    fun String.getCountryFlag(): String {
+        val flagOffset = 0x1F1E6
+        val asciiOffset = 0x41
+        val firstChar = Character.codePointAt(this, 0) - asciiOffset + flagOffset
+        val secondChar = Character.codePointAt(this, 1) - asciiOffset + flagOffset
+        val flag =
+            (String(Character.toChars(firstChar)) + String(Character.toChars(secondChar)))
+        return flag
+    }
+
     fun String.getDisplayName(): String {
         val locale = Locale(this)
         return locale.getDisplayName(locale)
